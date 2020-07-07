f297d890c9564d732f907dc57cf4417ecbb9d0b5
Leonid Trofymchuk <ltrofymchuk@evolutiongaming.com>
2020-07-07 23:32:04 +0300

    Vector magnitude/normalize

--- src/utils/vector_4.rs
+++ src/utils/vector_4.rs
@@ -1,17 +1,44 @@
-use std::ops::Neg;
+use std::ops::{Neg, Add, Sub, Mul, Div};
 
-#[derive(Debug)]
+pub trait RoundTo<T> {
+    fn round_to(&self, digits_after_comma: u8) -> T;
+}
+
+impl RoundTo<f32> for f32 {
+    fn round_to(&self, digits_after_comma: u8) -> Self {
+        (self * digits_after_comma as f32).round() / digits_after_comma as f32
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 pub struct Vector4 {
-    pub x: f64,
-    pub y: f64,
-    pub z: f64,
-    pub w: f64,
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub w: f32,
 }
 
 impl Vector4 {
-    pub fn new(x: f64, y: f64, z: f64, w: f64) -> Self {
+    pub fn new(x: f32, y: f32, z: f32, w: f32) -> Self {
         Vector4 {x, y, z, w}
     }
+}
+
+pub fn magnitude(v: Vector4) -> f32 {
+    (v.x * v.x + v.y * v.y + v.z * v.z + v.w * v.w).sqrt()
+}
+
+pub fn normalize(v: Vector4) -> Vector4 {
+    v / magnitude(v)
+}
+
+pub fn round_to(v: Vector4, digits_after_comma: u8) -> Vector4 {
+    Vector4::new(
+        v.x.round_to(digits_after_comma),
+        v.y.round_to(digits_after_comma),
+        v.z.round_to(digits_after_comma),
+        v.w.round_to(digits_after_comma),
+    )
 }
 
 impl PartialEq for Vector4 {
@@ -22,13 +49,54 @@ impl PartialEq for Vector4 {
 
 impl Neg for Vector4 {
     type Output = Vector4;
+
     fn neg(self) -> Self::Output {
         Vector4::new(self.x * -1.0, self.y * -1.0, self.z * -1.0, self.w * -1.0)
     }
 }
 
+impl Add for Vector4 {
+    type Output = Vector4;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vector4::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z, self.w + rhs.w)
+    }
+}
+
+impl Sub for Vector4 {
+    type Output = Vector4;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Vector4::new(self.x - rhs.x, self.y - rhs.y, self.z - rhs.z, self.w - rhs.w)
+    }
+}
+
+impl Mul<f32> for Vector4 {
+    type Output = Vector4;
+
+    fn mul(self, scalar: f32) -> Self::Output {
+        Vector4::new(self.x * scalar, self.y * scalar, self.z * scalar, self.w * scalar)
+    }
+}
+
+impl Mul for Vector4 {
+    type Output = Vector4;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Vector4::new(self.x * rhs.x, self.y * rhs.y, self.z * rhs.z, self.w * rhs.w)
+    }
+}
+
+impl Div<f32> for Vector4 {
+    type Output = Vector4;
+
+    fn div(self, scalar: f32) -> Self::Output {
+        Vector4::new(self.x / scalar, self.y / scalar, self.z / scalar, self.w / scalar)
+    }
+}
+
 #[macro_export]
-macro_rules! vector {
+macro_rules! vector4 {
     ($x: expr, $y: expr, $z: expr) => (
         Vector4::new($x, $y, $z, 0.0)
     );
--- tests/vector_4.rs
+++ tests/vector_4.rs
@@ -1,14 +1,14 @@
 #[macro_use]
 extern crate math_3d_rust;
-use math_3d_rust::utils::vector_4::Vector4;
+use math_3d_rust::utils::vector_4::{Vector4, normalize, magnitude, round_to};
 
 #[cfg(test)]
 mod vector_4 {
-    use super::{Vector4};
+    use super::{Vector4, normalize, magnitude, round_to};
 
     #[test]
     fn should_create_vector() {
-        let v = vector!(1.0, 2.0, 3.0);
+        let v = vector4!(1.0, 2.0, 3.0);
         assert_eq!(v.x, 1.0);
         assert_eq!(v.y, 2.0);
         assert_eq!(v.z, 3.0);
@@ -16,7 +16,85 @@ mod vector_4 {
 
     #[test]
     fn should_negate_vector() {
-        let v = vector!(1.0, -2.0, 3.0);
-        assert_eq!(-v, vector!(-1.0, 2.0, -3.0));
+        let v = vector4!(1.0, -2.0, 3.0);
+        assert_eq!(-v, vector4!(-1.0, 2.0, -3.0));
+    }
+
+    #[test]
+    fn should_add_two_vectors() {
+        let a = vector4!(-1.0, 2.0, -3.0);
+        let b = vector4!(5.0, -6.0, 1.0);
+        assert_eq!(a + b, vector4!(4.0, -4.0, -2.0));
+    }
+
+    #[test]
+    fn should_subtract_two_vectors() {
+        let a = vector4!(-1.0, 2.0, -3.0);
+        let b = vector4!(5.0, -6.0, 1.0);
+        assert_eq!(a - b, vector4!(-6.0, 8.0, -4.0));
+    }
+
+    #[test]
+    fn should_multiply_vector_by_scalar() {
+        let v = vector4!(1.0, -2.0, 3.0);
+        assert_eq!(v * 0.5, vector4!(0.5, -1.0, 1.5));
+    }
+
+    #[test]
+    fn should_multiply_vector_by_vector() {
+        let a = vector4!(-1.0, 0.0, 2.0);
+        let b = vector4!(2.0, 0.0, 4.0);
+        assert_eq!(a * b, vector4!(-2.0, 0.0, 8.0));
+    }
+
+    #[test]
+    fn should_divide_vector_by_scalar() {
+        let v = vector4!(1.0, -2.0, 3.0);
+        assert_eq!(v / 2.0, vector4!(0.5, -1.0, 1.5));
+    }
+
+    #[test]
+    fn should_calculate_vector_magnitude_1() {
+        let v = vector4!(1.0, 0.0, 0.0);
+        assert_eq!(magnitude(v), 1.0);
+    }
+
+    #[test]
+    fn should_calculate_vector_magnitude_2() {
+        let v = vector4!(0.0, 1.0, 0.0);
+        assert_eq!(magnitude(v), 1.0);
+    }
+
+    #[test]
+    fn should_calculate_vector_magnitude_3() {
+        let v = vector4!(0.0, 0.0, 1.0);
+        assert_eq!(magnitude(v), 1.0);
+    }
+
+    #[test]
+    fn should_calculate_vector_magnitude_4() {
+        let v = vector4!(1.0, 5.0, 2.0);
+        assert_eq!(magnitude(v), 30.0_f32.sqrt());
+    }
+
+    #[test]
+    fn should_calculate_vector_magnitude_5() {
+        let v = vector4!(-1.0, -4.0, -8.0);
+        assert_eq!(magnitude(v), 9.0);
+    }
+
+    #[test]
+    fn should_normalize_vector() {
+        let v = vector4!(-1.0, -4.0, -8.0);
+        assert_eq!(
+            round_to(normalize(v),4),
+            round_to(vector4!(-0.0, -0.5, -1.0), 4),
+        );
+    }
+
+    #[test]
+    fn should_magnitude_of_normalized_vector_1() {
+        let v = vector4!(-1.0, -4.0, -8.0);
+        assert_eq!(magnitude(normalize(v)), 1.0);
     }
 }
